6be39b8d47a290c06164ec3ce305dc59779d013b
galangprihadi <galang.prihadi@gmail.com>
2025-09-22 09:11:33 +0900

    Refactor CSS styles for button layout and update form submission script

--- js/script.js
+++ js/script.js
@@ -12,39 +12,3 @@ const lib = {
         this.gameTransition.addEventListener("animationend", handlerStart);
     },
 };
-
-class Utama {
-    constructor(param) {
-        this.btnKirim = document.getElementById("button-kirim");
-
-        this.formAction = param.formAction;
-        this.entry1 = param.entry1;
-        this.entry2 = param.entry2;
-
-        this.btnKirim.addEventListener("click", () => {
-            this.submitForm();
-        });
-    }
-
-    async submitForm() {
-        const playerId = "John Doe";
-        const playerData = "abcd1234";
-
-        const formData = new FormData();
-        formData.append(this.entry1, playerId);
-        formData.append(this.entry2, playerData);
-
-        try {
-            await fetch(this.formAction, {
-                method: 'POST',
-                mode: 'no-cors',
-                body: formData,
-            });
-
-            console.log("Seharusnya sudah terkirim!");
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-}
