0776bb590ab2ce9826f3bc37058e1b4de8efcf16
galangprihadi <galang.prihadi@gmail.com>
2025-10-06 09:48:29 +0900

    Add demo pages and game logic for remote control and programming modes

---
+++ demo/remote-demo.js
@@ -0,0 +1,46 @@
+const gameSet = {
+    tileSize : 80,
+    tileWidth : 6,
+    tileHeight : 5,
+    playerAniCycle : 2,
+    playerDelay : 5,
+    goalAniCycle : 12,
+    obsAniCycle : 12,
+    defaultMode : "Tap Mode",
+    
+    // inputType => "Tap Mode" "Scroll Mode" "Flick Mode" "Stamp Mode" "Drag and Drop"
+
+    level1: {
+        map: "../images/map.png",
+        commandLength: 8,
+        inputType: "Tap Mode",
+        player: {x:2, y:2, direction: "down", image: "../images/cat.png"},
+        goals: {
+            goal1: {x:4, y:2, image: "../images/goalFish.png"},
+        },
+        obstacles: {
+            obs1: {x:0, y:4, image: "../images/obsBox.png"},
+            obs2: {x:1, y:3, image: "../images/obsBox.png"},
+            obs3: {x:3, y:2, image: "../images/obsGrass.png"},
+            obs4: {x:4, y:1, image: "../images/obsGrass.png"},
+            obs5: {x:5, y:0, image: "../images/obsGrass.png"},
+        }
+    },
+
+    level2: {
+        map: "../images/map.png",
+        commandLength: 8,
+        inputType: "Scroll Mode",
+        player: {x:2, y:3, direction: "down", image: "../images/cat.png"},
+        goals: {
+            goal1: {x:4, y:1, image: "../images/goalFish.png"},
+        },
+        obstacles: {
+            obs1: {x:2, y:2, image: "../images/obsBox.png"},
+            obs2: {x:3, y:1, image: "../images/obsBox.png"},
+            obs3: {x:3, y:2, image: "../images/obsBox.png"},
+            obs4: {x:1, y:3, image: "../images/obsGrass.png"},
+            obs5: {x:2, y:0, image: "../images/obsGrass.png"},
+        }
+    },
+}
